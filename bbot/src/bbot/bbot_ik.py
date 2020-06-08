ac7a6209b17749553e85d9f26704d360eb62fa9b
Ashtin Cheng <ashtin72@ucla.edu>
2020-06-07 19:13:41 -0700

    Updated IK to account for world to zero frame translation, tuned joint 1 gains

--- bbot/src/bbot/bbot_ik.py
+++ bbot/src/bbot/bbot_ik.py
@@ -49,6 +49,15 @@ class bbotAnalysis():
 		self.alpha = [0, math.pi/2., 0, 0, math.pi/2., math.pi/2.]
 		self.d = [0, 0, 0, self.d4, 0, 0]
 
+		self.a0 = 0.0133
+		self.d0 = 0.09193
+
+		self.worldto0 = np.matrix([
+			[1,0,0,self.a0],
+			[0,1,0,0],
+			[0,0,1,self.d0],
+			[0,0,0,1]])
+
 		self.d7 = .231
 		self.transtoEE = np.matrix([
 			[1,0,0,0],
@@ -86,8 +95,8 @@ class bbotAnalysis():
 			configlist = self.possconfigs
 		else:
 			configlist = configoption
-		self.targetPose = pose*np.linalg.inv(self.transtoEE)
-		pose = pose*np.linalg.inv(self.transtoEE)
+		self.targetPose = np.linalg.inv(self.worldto0)*pose*np.linalg.inv(self.transtoEE)
+		pose = np.linalg.inv(self.worldto0)*pose*np.linalg.inv(self.transtoEE)
 		solnstemp = []
 		configstemp = []
 
@@ -186,7 +195,6 @@ class bbotAnalysis():
 				if verbose == True:
 					print(str(configlist[i]) + " failed because Joint 4 violated with command " + str(t4))
 				continue
-
 			if equivalenceCheck(self.FK([t1, t2, t3, t4, t5, t6]),self.targetPose):
 				solnstemp.append([t1,t2,t3,t4,t5,t6])
 				configstemp.append(configlist[i])
@@ -258,11 +266,11 @@ if __name__ == '__main__':
 	# t3 = -pi/2
 
 	M = np.matrix([
-		[0,0,1,0.4],
-		[-1,0,0,-0.1],
-		[0,-1,0,-0.1],
+		[1,0,0,0.0625],
+		[0,0,1,0.365],
+		[0,-1,0,0.105],
 		[0,0,0,1]
-		]) # cup1
+		])
 
 	# M = np.matrix([
 	# 	[-.7071,0.5,0.5,0.2304],
